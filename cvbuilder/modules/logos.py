d337be2bb7da5e0660b8ceb2f3f4fc9568ff4509
Gaëtan Staquet <gaetan.staquet@gmail.com>
2023-09-10 20:53:21 +0200

    Logos: module
    Summary: removed logos

---
+++ cvbuilder/modules/logos.py
@@ -0,0 +1,53 @@
+from typing import List
+
+from .. import modules as mod
+from .. import contexts
+
+
+class Logos(mod.Data):
+    def __init__(
+        self,
+        logos: List[str],
+    ):
+        self.logos = logos
+
+    def to_latex(self, context: contexts.latex.LaTeXContext) -> str:
+        raise NotImplementedError(
+            "Logos module does not support the LaTeX context"
+        )
+
+    def to_html(self, context: contexts.html.HTMLContext) -> str:
+        html = context.open_div("logos")
+
+        html += context.open_div("align")
+
+        for index, logo in enumerate(self.logos):
+            html += context.img_block(f"logo{index}", logo, "")
+
+        html += context.close_block()  # align
+
+        html += context.close_block()  # logos
+
+        return html
+
+
+class LogosModule(mod.Module):
+    def __init__(
+        self,
+    ):
+        super().__init__(0, "", "")
+
+    def load(self, json_value) -> None:
+        self.data.append((None, [self._load(json_value)]))
+
+    def to_latex(self, context: contexts.latex.LaTeXContext) -> str:
+        raise NotImplementedError("Logos module is not implemented for LaTeX")
+
+    def to_html(self, context: contexts.html.HTMLContext) -> str:
+        return self.data[0][1][0].to_html(context)
+
+    def _load(self, json_object) -> Logos:
+        return Logos(json_object)
+
+    def _get_class_name(self) -> str:
+        return "summary"
--- cvbuilder/modules/summary.py
+++ cvbuilder/modules/summary.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from .. import modules as mod
 from .. import contexts
 
@@ -7,20 +5,12 @@ from .. import contexts
 class Summary(mod.Data):
     def __init__(
         self,
-        logos: List[str],
         text: mod.Description,
-        level: int,
-        section: str,
-        icon: str,
     ):
-        self.logos = logos
         if text is not None:
             self.text = mod.Description(text)
         else:
             self.text = None
-        self.level = level
-        self.section = section
-        self.icon = icon
 
     def to_latex(self, context: contexts.latex.LaTeXContext) -> str:
         raise NotImplementedError(
@@ -28,22 +18,7 @@ class Summary(mod.Data):
         )
 
     def to_html(self, context: contexts.html.HTMLContext) -> str:
-        html = context.open_div("logos")
-
-        html += context.open_div("align")
-
-        for index, logo in enumerate(self.logos):
-            html += context.img_block(f"logo{index}", logo, "")
-
-        html += context.close_block()  # align
-
-        html += context.close_block()  # logos
-
-        html += context.open_section(self.level, self.section, "summary", self.icon)
-
-        html += context.simple_div_block("details", self.text)
-
-        html += context.close_block()
+        html = context.simple_div_block("details", self.text)
 
         return html
 
@@ -60,16 +35,8 @@ class SummaryModule(mod.Module):
     def load(self, json_value) -> None:
         self.data.append((None, [self._load(json_value)]))
 
-    def to_latex(self, context: contexts.latex.LaTeXContext) -> str:
-        raise NotImplementedError("Summary module is not implemented for LaTeX")
-
-    def to_html(self, context: contexts.html.HTMLContext) -> str:
-        return self.data[0][1][0].to_html(context)
-
     def _load(self, json_object) -> Summary:
-        logos = json_object["logos"] if "logos" in json_object else []
-        text = json_object["text"] if "text" in json_object else None
-        return Summary(logos, text, self.level + 1, self.section, self.section_icon)
+        return Summary(json_object)
 
     def _get_class_name(self) -> str:
         return "summary"
