d6700a697a410cd7a97930414dbeaab42919873d
Gaëtan Staquet <gaetan.staquet@gmail.com>
2023-10-07 12:54:07 +0200

    Simplified main and CSS files

--- main.py
+++ main.py
@@ -15,80 +15,78 @@ from cvbuilder.modules.project import ProjectModule
 from cvbuilder.modules.summary import SummaryModule
 from cvbuilder.modules.logos import LogosModule
 from cvbuilder.modules.event import EventModule
+from cvbuilder.modules.award import AwardModule
 
+# Copy resources folder
+copytree("resources/", "output/html/resources/", dirs_exist_ok=True)
 
-if __name__ == "__main__":
-    # Copy resources folder
-    copytree("resources/", "output/html/resources/", dirs_exist_ok=True)
+# HTML (producing two different files)
+builder = Builder()
 
-    # HTML (producing two different files)
-    builder = Builder()
+html = HTMLContext("output/html/index.html")
+html.add_css_file("resources/css/style.css")
+html.add_css_file("resources/css/sidebar.css")
+html.add_css_file("resources/css/summary.css")
 
-    html = HTMLContext("output/html/index.html")
-    html.add_css_file("resources/css/main.css")
-    html.add_css_file("resources/css/sidebar.css")
-    html.add_css_file("resources/css/item.css")
-    html.add_css_file("resources/css/summary.css")
+builder.add_context(html)
 
-    builder.add_context(html)
+builder.add_module("logos", LogosModule())
+builder.add_module("summary", SummaryModule())
+builder.add_module("work", JobModule())
+builder.add_module(
+    None,
+    LinkModule(
+        section="Publications",
+        before="Consult the ",
+        link="publications.html",
+        text="Publications",
+        after=" page.",
+        icon="iconoir-journal"
+    ),
+)
+builder.add_module("talks", TalkModule())
+builder.add_module("teaching", TeachModule())
+builder.add_module("supervision", SupervisionModule())
+builder.add_module("projects", ProjectModule())
+builder.add_module("events", EventModule())
+builder.add_module("awards", AwardModule())
+builder.add_module(
+    None,
+    TextModule(
+        section="Closing Words",
+        text="Doctor, I let you go.",
+    ),
+)
 
-    builder.add_module("logos", LogosModule())
-    builder.add_module("summary", SummaryModule())
-    builder.add_module("work", JobModule())
-    builder.add_module(
-        None,
-        LinkModule(
-            section="Publications",
-            before="Consult the ",
-            link="publications.html",
-            text="Publications",
-            after=" page.",
-            icon="iconoir-journal"
-        ),
-    )
-    builder.add_module("talks", TalkModule())
-    builder.add_module("teaching", TeachModule())
-    builder.add_module("supervision", SupervisionModule())
-    builder.add_module("projects", ProjectModule())
-    builder.add_module("events", EventModule())
-    builder.add_module(
-        None,
-        TextModule(
-            section="Closing Words",
-            text="Doctor, I let you go.",
-        ),
-    )
+builder.build("example.json")
 
-    builder.build("example.json")
+builder = Builder()
 
-    builder = Builder()
+html = HTMLContext("output/html/publications.html")
+html.set_title_fct(lambda personal: f"{personal.name} - Publications")
 
-    html = HTMLContext("output/html/publications.html")
-    html.set_title_fct(lambda personal: f"{personal.name} - Publications")
+html.add_css_file("resources/css/style.css")
+html.add_css_file("resources/css/sidebar.css")
 
-    html.add_css_file("resources/css/main.css")
-    html.add_css_file("resources/css/sidebar.css")
-    html.add_css_file("resources/css/item.css")
+builder.add_context(html)
 
-    builder.add_context(html)
+builder.add_module("publications", PublicationModule())
 
-    builder.add_module("publications", PublicationModule())
+builder.build("example.json")
 
-    builder.build("example.json")
+# Example for LaTeX
+# TODO: add Markdown context
+builder = Builder()
+latex = LaTeXContext("output/latex/example.tex")
+latex.set_style("title", Style({"author": "\\bfseries"}))
 
-    # Example for LaTeX
-    # TODO: add Markdown context
-    builder = Builder()
-    latex = LaTeXContext("output/latex/example.tex")
-    latex.set_style("title", Style({"author": "\\bfseries"}))
+builder.add_context(latex)
 
-    builder.add_context(latex)
+builder.add_module("work", JobModule())
+builder.add_module("publications", PublicationModule())
+builder.add_module("talks", TalkModule())
+builder.add_module("teaching", TeachModule())
+builder.add_module("supervision", SupervisionModule())
+builder.add_module("projects", ProjectModule())
 
-    builder.add_module("work", JobModule())
-    builder.add_module("publications", PublicationModule())
-    builder.add_module("talks", TalkModule())
-    builder.add_module("teaching", TeachModule())
-    builder.add_module("supervision", SupervisionModule())
-    builder.add_module("projects", ProjectModule())
-
-    builder.build(Path("example.json"))
+builder.build(Path("example.json"))
